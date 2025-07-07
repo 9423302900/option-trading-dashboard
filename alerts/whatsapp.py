fc11291ec1759510570b6bc85fb1974aad2f6b77
9423302900 <ganeshkoli24@gmail.com>
2025-07-07 21:57:35 +0500

    Update whatsapp.py

--- alerts/whatsapp.py
+++ alerts/whatsapp.py
@@ -1,10 +1,10 @@
 from twilio.rest import Client
 
 def send_whatsapp_alert(message):
-    account_sid = "your_twilio_sid"
-    auth_token = "your_twilio_auth_token"
+    account_sid = "HXb5b62575e6e4ff6129ad7c8efe1f983e'"
+    auth_token = "A"
     from_whatsapp = "whatsapp:+14155238886"  # Twilio sandbox
-    to_whatsapp = "whatsapp:+91YOURNUMBER"
+    to_whatsapp = "whatsapp:+919423302900"
 
     client = Client(account_sid, auth_token)
     client.messages.create(
